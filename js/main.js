1604061ef77e3be6d4b3ce6692e85bad200bc7c1
Zephyr Pellerin <zv@nxvr.org>
2016-07-09 18:20:50 -0700

    synthesis should initial from meteorological data

--- js/main.js
+++ js/main.js
@@ -19,19 +19,77 @@ var Y = 0;
 var mouseState = false;
 var helpvisible = true;
 
-//control initial settings
+// control initial settings
 var attack = 0.40;
 var release = 0.40;
 var density = 0.85;
-var spread = 0.2;
+var spread = 0.2; // Spread between grain start and end
 var reverb = 0.5;
 var pan = 0.1;
 var trans = 1;
 
+var queryInterval = 2000; // Frequency we query the current sound file (in millisecnds)
+
+
+const owAPIKey = "033c9c2e06f99aef2fe57093880b686f";
+
+var startTime = 1459753200;
+var endTime = 1459839600;
+
+var grainDuration = spread;
+var grainSpacing = 0.5 * spread;
+
+var latitudeTulsa = 36.15;
+var longitudeTulsa = 95.99;
+
+var latitudeSF = 37.77;
+var longitudeSF = 122.41;
+
+var speed = 0.1;
+
+// This function queries openweathermap for data
+function weatherFromCoordinates(latitude, longitude, callback) {
+    var reqURL = `http://api.openweathermap.org/data/2.5/weather?q=lat=${latitude}&$lon=${longitude}&appid=033c9c2e06f99aef2fe57093880b686f`;
+    fetch(reqURL).then(function(response) {
+        // Convert to JSON
+        return response.json();
+    }).then(callback);
+}
+
+// This is called when the API returns it's update
+function updateParameters(result) {
+
+    let = {
+        main: {
+            temp: temperature,
+            humidity: humidity,
+            pressure: pressure
+        }
+    } = result
+
+    // Update parameters here
+    spread = convertHumidityToSpread(humidity)
+    speed  = convertKelvinToGrainSeconds(temperature);
+    queryInterval = converthPaToGrainRandomSpeed(pressure);
+
+    console.log("spread", spread)
+    console.log("queryInterva", queryInterval)
+}
+
+function convertHumidityToSpread(value) {
+    return value / 100;
+}
+
+function convertKelvinToGrainSeconds(value) {
+    return ((value - 200) / (350 - 200)) * (3 - 0.01) + 0.01;
+}
+
+function converthPaToGrainRandomSpeed(value) {
+    return ((value - 800) / (1100 - 800)) * (3000 - 700) + 700;
+}
 
 //the grain class
 function grain(p,buffer,positionx,positiony,attack,release,spread,pan){
-
 	var that = this; //for scope issues
 	this.now = context.currentTime; //update the time value
 	//create the source
@@ -40,7 +98,7 @@ function grain(p,buffer,positionx,positiony,attack,release,spread,pan){
 	this.source.buffer = buffer;
 	//create the gain for enveloping
 	this.gain = context.createGain();
-	
+
 	//experimenting with adding a panner node - not all the grains will be panned for better performance
 	var yes = parseInt(p.random(3),10);
 	if( yes === 1){
@@ -54,37 +112,37 @@ function grain(p,buffer,positionx,positiony,attack,release,spread,pan){
 	}else{
 		this.source.connect(this.gain);
 	}
-	
-	
+
+
 	this.gain.connect(master);
-	
+
 	//update the position and calcuate the offset
 	this.positionx = positionx;
 	this.offset = this.positionx * (buffer.duration / w); //pixels to seconds
-	
+
 	//update and calculate the amplitude
 	this.positiony = positiony;
 	this.amp = this.positiony / h;
 	this.amp = p.map(this.amp,0.0,1.0,1.0,0.0) * 0.7;
-	
+
 	//parameters
 	this.attack = attack * 0.4;
 	this.release = release * 1.5;
-	
+
 	if(this.release < 0){
 		this.release = 0.1; // 0 - release causes mute for some reason
 	}
 	this.spread = spread;
 
-	this.randomoffset = (Math.random() * this.spread) - (this.spread / 2); //in seconds
+	  this.randomoffset = (Math.random() * this.spread) - (this.spread / 2); //in seconds
 	///envelope
 	this.source.start(this.now,this.offset + this.randomoffset,this.attack + this.release); //parameters (when,offset,duration)
 	this.gain.gain.setValueAtTime(0.0, this.now);
 	this.gain.gain.linearRampToValueAtTime(this.amp,this.now + this.attack);
 	this.gain.gain.linearRampToValueAtTime(0,this.now + (this.attack +  this.release) );
-	
+
 	//garbage collection
-	this.source.stop(this.now + this.attack + this.release + 0.1); 
+	this.source.stop(this.now + this.attack + this.release + 0.1);
 	var tms = (this.attack + this.release) * 1000; //calculate the time in miliseconds
 	setTimeout(function(){
 		that.gain.disconnect();
@@ -109,293 +167,329 @@ function grain(p,buffer,positionx,positiony,attack,release,spread,pan){
 
 //the voice class
 function voice(id){
-	
-	this.touchid = id; //the id of the touch event 
+
+	this.touchid = id; //the id of the touch event
 }
+
+weatherFromCoordinates(latitudeTulsa, longitudeTulsa, updateParameters);
+
+
+var currentPosition = 1000;
+
+
+function playNext(p) {
+    var positionX = currentPosition + Math.pow((1+spread), 3);
+    this.grains = [];
+    this.grainscount = 0;
+    var that = this; //for scope issues
+    this.play = function(){
+        //create new grain
+        var g = new grain(p,buffer,positionX,50,attack,release,spread,pan);
+        //push to the array
+        that.grains[that.graincount] = g;
+        that.graincount+=1;
+
+        if(that.graincount > 20){
+            that.graincount = 0;
+        }
+        //next interval
+        this.dens = p.map(density,1,0,0,1);
+        this.interval = (this.dens * 500) + 70;
+        that.timeout = setTimeout(that.play,this.interval);
+
+    }
+    currentPosition = positionX
+    this.play();
+}
+
+
+
 
 //play function for mouse event
 voice.prototype.playmouse = function(p){
-	this.grains = [];
-	this.grainscount = 0;
-	var that = this; //for scope issues	
-	this.play = function(){
-		//create new grain
-		var g = new grain(p,buffer,p.mouseX,p.mouseY,attack,release,spread,pan);
-		//push to the array
-		that.grains[that.graincount] = g;
-		that.graincount+=1;
-				
-		if(that.graincount > 20){
-			that.graincount = 0;
-		}
-		//next interval
-		this.dens = p.map(density,1,0,0,1);
-		this.interval = (this.dens * 500) + 70;
-		that.timeout = setTimeout(that.play,this.interval);
-		
-	}
-	this.play();
+    setInterval(() => playNext(p), queryInterval)
+    return;
+    this.grains = [];
+    this.grainscount = 0;
+    var that = this; //for scope issues
+    this.play = function(){
+        //create new grain
+        var g = new grain(p,buffer,p.mouseX,p.mouseY,attack,release,spread,pan);
+        //push to the array
+        that.grains[that.graincount] = g;
+        that.graincount+=1;
+
+        if(that.graincount > 20){
+            that.graincount = 0;
+        }
+        //next interval
+        this.dens = p.map(density,1,0,0,1);
+        this.interval = (this.dens * 500) + 70;
+        that.timeout = setTimeout(that.play,this.interval);
+
+    }
+    this.play();
 }
 //play function for touch events - this will get the position from touch events
 voice.prototype.playtouch = function(p,positionx,positiony){
-	//this.positiony = positiony;
-	this.positionx = positionx;
-	this.positiony = positiony;
-	this.grains = [];
-	this.graincount = 0;
+    //this.positiony = positiony;
+    this.positionx = positionx;
+    this.positiony = positiony;
+    this.grains = [];
+    this.graincount = 0;
 
-	var that = this; //for scope issues	
-	this.play = function(){
-		//create new grain
-		var g = new grain(p,buffer,that.positionx,that.positiony,attack,release,spread,pan);
+    var that = this; //for scope issues
+    this.play = function(){
+        //create new grain
+        var g = new grain(p,buffer,that.positionx,that.positiony,attack,release,spread,pan);
 
-		//push to the array
-		that.grains[that.graincount] = g;
-		that.graincount+=1;
-				
-		if(that.graincount > 30){
-			that.graincount = 0;
-		}
-		//next interval
-		this.dens = p.map(density,1,0,0,1);
-		this.interval = (this.dens * 500) + 70;
-		that.timeout = setTimeout( that.play, this.interval );
-	}
-	this.play();
+        //push to the array
+        that.grains[that.graincount] = g;
+        that.graincount+=1;
+
+        if(that.graincount > 30){
+            that.graincount = 0;
+        }
+        //next interval
+        this.dens = p.map(density,1,0,0,1);
+        this.interval = (this.dens * 500) + 70;
+        that.timeout = setTimeout( that.play, this.interval );
+    }
+    this.play();
 }
 
 //stop method
 voice.prototype.stop = function(){
-	clearTimeout(this.timeout);
+    clearTimeout(this.timeout);
 }
 
 //loading the first sound with XML HTTP REQUEST
 var request = new XMLHttpRequest();
-	request.open('GET','audio/guitar.mp3',true);
-	request.responseType = "arraybuffer";
-	request.onload = function(){
-		context.decodeAudioData(request.response,function(b){
-			buffer = b; //set the buffer
-			data = buffer.getChannelData(0);
-			isloaded = true;
-			var canvas1 = document.getElementById('canvas');
-			//initialize the processing draw when the buffer is ready
-			var processing = new Processing(canvas1,waveformdisplay);
+request.open('GET','audio/guitar.mp3',true);
+request.responseType = "arraybuffer";
+request.onload = function(){
+    context.decodeAudioData(request.response,function(b){
+        buffer = b; //set the buffer
+        data = buffer.getChannelData(0);
+        isloaded = true;
+        var canvas1 = document.getElementById('canvas');
+        //initialize the processing draw when the buffer is ready
+        var processing = new Processing(canvas1,waveformdisplay);
 
-		},function(){
-			console.log('loading failed')
-		});
-	};
+    },function(){
+        console.log('loading failed')
+    });
+};
 request.send();
 
 
-//processing - waveform display - canvas 
+//processing - waveform display - canvas
 function waveformdisplay(p){
-	w = parseInt($('#waveform').css('width'),10); //get the width
-	h = parseInt($('#waveform').css('height'),10); //get the height
+    w = parseInt($('#waveform').css('width'),10); //get the width
+    h = parseInt($('#waveform').css('height'),10); //get the height
 
-	//draw the buffer
-	function drawBuffer() {
-	    var step = Math.ceil( data.length / w );
-	    var amp = h / 2;
-	    
-	    p.background(0);
-	    for( var i=0; i < w; i++ ){
-	        var min = 1.0;
-	        var max = -1.0;
-	     
-	        for( j=0; j<step; j++) {
-	            
-	            var datum = data[(i*step)+j]; 
-	            if (datum < min){
-	            	min = datum;
-	            }else if(datum > max){
-	            	max = datum;
-	            }
-	                       
-	        }
-	        //p.stroke(p.random(255),p.random(255),p.random(255));
-	       	p.rect(i,(1+min)*amp,1,Math.max(1,(max-min)*amp));
-	    }
-	}
-	
-	p.setup = function(){
-		p.size(w,h);
-		p.background(0);//background black	
-		
-		//change the size on resize
-		$(window).resize(function(){
-			w = parseInt($('#waveform').css('width'),10);
-			h = parseInt($('#waveform').css('height'),10);
-			p.size(w,h);
-			//redraw buffer on resize
-			p.stroke(255);
-			drawBuffer();
+    //draw the buffer
+    function drawBuffer() {
+        var step = Math.ceil( data.length / w );
+        var amp = h / 2;
 
-		});
-		p.strokeWeight(0.01);
-		p.stroke(255);
-		drawBuffer();
-		p.noLoop();
+        p.background(0);
+        for( var i=0; i < w; i++ ){
+            var min = 1.0;
+            var max = -1.0;
 
-	};
-	
+            for( j=0; j<step; j++) {
+
+                var datum = data[(i*step)+j];
+                if (datum < min){
+                    min = datum;
+                }else if(datum > max){
+                    max = datum;
+                }
+
+            }
+            //p.stroke(p.random(255),p.random(255),p.random(255));
+            p.rect(i,(1+min)*amp,1,Math.max(1,(max-min)*amp));
+        }
+    }
+
+    p.setup = function(){
+        p.size(w,h);
+        p.background(0);//background black
+
+        //change the size on resize
+        $(window).resize(function(){
+            w = parseInt($('#waveform').css('width'),10);
+            h = parseInt($('#waveform').css('height'),10);
+            p.size(w,h);
+            //redraw buffer on resize
+            p.stroke(255);
+            drawBuffer();
+
+        });
+        p.strokeWeight(0.01);
+        p.stroke(255);
+        drawBuffer();
+        p.noLoop();
+
+    };
+
 }
 
 //processing - grain display and main interaction system
 function grainsdisplay(p){
-	w = parseInt($('#waveform').css('width'),10);
-	h = parseInt($('#waveform').css('height'),10);
+    w = parseInt($('#waveform').css('width'),10);
+    h = parseInt($('#waveform').css('height'),10);
 
-	//setup
-	p.setup = function(){
-		p.size(w,h);
-		p.background(0,0);//backgorund black alpha 0
-		p.frameRate(24);
-		p.noLoop();
-		
-		//change the size on resize
-		$(window).resize(function(){
-			w = parseInt($('#waveform').css('width'),10);
-			h = parseInt($('#waveform').css('height'),10);
-			p.size(w,h);
+    //setup
+    p.setup = function(){
+        p.size(w,h);
+        p.background(0,0);//backgorund black alpha 0
+        p.frameRate(24);
+        p.noLoop();
 
-		});	
-		
-	};
-	
+        //change the size on resize
+        $(window).resize(function(){
+            w = parseInt($('#waveform').css('width'),10);
+            h = parseInt($('#waveform').css('height'),10);
+            p.size(w,h);
 
-	//mouse events
-	$('#canvas2').mousedown(function(){
-		mouseState = true;
-		
-		if(mouseState){
-			var v = new voice();
-			v.playmouse(p);
-			voicesmono[0] = v; //have in the array
-		}
-	}).mouseup(function(){
-		mouseState = false;
-		for(var i = 0; i < voicesmono.length;i++){
-			voicesmono[i].stop();
-			voicesmono.splice(i);
-		}
-		setTimeout(function(){
-			p.background();
-		},300);
-	}).mousemove(function(){
-		X = p.mouseX;
-		Y = p.mouseY;
-		
-	});
-	//safety for when the mouse is out of the canvas
-	$(document).mousemove(function(e){
-		if(e.target.id !== 'canvas2'){
-			for(var i = 0; i < voicesmono.length;i++){
-				voicesmono[i].stop();
-				voicesmono.splice(i);
-				setTimeout(function(){
-					p.background();
-				},300);
-			}
-		}
-	});
+        });
 
-	//touch events
-	var canvas2 = document.getElementById('canvas2');
-	canvas2.addEventListener('touchstart',function(event){
-		
-		event.preventDefault(); //to prevent scrolling
-		
-		//4 touches glitches on ipad
-		if(event.touches.length < 4){
+    };
 
-			for(var i = 0; i < event.touches.length; i++){
-				
-				if(event.touches[i].target.id === 'canvas2'){
-					var id = event.touches[i].identifier; //the id will be used for voice stop
-					var v = new voice(id); 
-					var clientX = event.touches[i].clientX;
-					var clientY = event.touches[i].clientY;
-					
-					//multitouch optimization
-					var interval;
-					//calculate the reverse interval
-					if(event.touches.length > 1){
-						interval = p.map(density,0,1,1,0.7);
-					}else{
-						interval = p.map(density,0,1,1,0);
-					}
-					 
-					//play
-					v.playtouch(p,clientX,clientY,interval);
-					voices.push(v);
-				}	
-			}
-		}	
-	});
 
-	canvas2.addEventListener('touchend',function(event){
-		
-		for(var i = 0; i < voices.length; i++){
-			
-			for(var j = 0; j < event.changedTouches.length;j++){
+    //mouse events
+    $('#canvas2').mousedown(function(){
+        mouseState = true;
 
-				if(voices[i].touchid === event.changedTouches[j].identifier){
-					
-					voices[i].stop();
-					
-				}
-			}
-		}	
-		
-		//safety and garbage collection
-		if(event.touches.length < 1){
-			for(var i = 0; i < voices.length; i++){
-				voices[i].stop();
-			}
-			voices = [];
-			setTimeout(function(){
-				p.background();
-				
-			},200);
-		}
-		
-		
-	});
-	
-	canvas2.addEventListener('touchmove',function(event){
-		event.preventDefault();
-		
-		for(var i = 0; i < voices.length; i++){
-			
-			for(var j = 0; j < event.changedTouches.length;j++){
-				
-				if(voices[i].touchid === event.changedTouches[j].identifier){
-					if(event.changedTouches[j].clientY < h + 50){
-						voices[i].positiony = event.changedTouches[j].clientY;
-						voices[i].positionx = event.changedTouches[j].clientX;	
-					}else{
-						voices[i].stop();
-					}
-				}
-			}
-		}
-	});
+        if(mouseState){
+            var v = new voice();
+            v.playmouse(p);
+            voicesmono[0] = v; //have in the array
+        }
+    }).mouseup(function(){
+        mouseState = false;
+        for(var i = 0; i < voicesmono.length;i++){
+            voicesmono[i].stop();
+            voicesmono.splice(i);
+        }
+        setTimeout(function(){
+            p.background();
+        },300);
+    }).mousemove(function(){
+        X = p.mouseX;
+        Y = p.mouseY;
+
+    });
+    //safety for when the mouse is out of the canvas
+    $(document).mousemove(function(e){
+        if(e.target.id !== 'canvas2'){
+            for(var i = 0; i < voicesmono.length;i++){
+                voicesmono[i].stop();
+                voicesmono.splice(i);
+                setTimeout(function(){
+                    p.background();
+                },300);
+            }
+        }
+    });
+
+    //touch events
+    var canvas2 = document.getElementById('canvas2');
+    canvas2.addEventListener('touchstart',function(event){
+
+        event.preventDefault(); //to prevent scrolling
+
+        //4 touches glitches on ipad
+        if(event.touches.length < 4){
+
+            for(var i = 0; i < event.touches.length; i++){
+
+                if(event.touches[i].target.id === 'canvas2'){
+                    var id = event.touches[i].identifier; //the id will be used for voice stop
+                    var v = new voice(id);
+                    var clientX = event.touches[i].clientX;
+                    var clientY = event.touches[i].clientY;
+
+                    //multitouch optimization
+                    var interval;
+                    //calculate the reverse interval
+                    if(event.touches.length > 1){
+                        interval = p.map(density,0,1,1,0.7);
+                    }else{
+                        interval = p.map(density,0,1,1,0);
+                    }
+
+                    //play
+                    v.playtouch(p,clientX,clientY,interval);
+                    voices.push(v);
+                }
+            }
+        }
+    });
+
+    canvas2.addEventListener('touchend',function(event){
+
+        for(var i = 0; i < voices.length; i++){
+
+            for(var j = 0; j < event.changedTouches.length;j++){
+
+                if(voices[i].touchid === event.changedTouches[j].identifier){
+
+                    voices[i].stop();
+
+                }
+            }
+        }
+
+        //safety and garbage collection
+        if(event.touches.length < 1){
+            for(var i = 0; i < voices.length; i++){
+                voices[i].stop();
+            }
+            voices = [];
+            setTimeout(function(){
+                p.background();
+
+            },200);
+        }
+
+
+    });
+
+    canvas2.addEventListener('touchmove',function(event){
+		    event.preventDefault();
+
+		    for(var i = 0; i < voices.length; i++){
+
+			      for(var j = 0; j < event.changedTouches.length;j++){
+
+				        if(voices[i].touchid === event.changedTouches[j].identifier){
+					          if(event.changedTouches[j].clientY < h + 50){
+						            voices[i].positiony = event.changedTouches[j].clientY;
+						            voices[i].positionx = event.changedTouches[j].clientX;
+					          }else{
+						            voices[i].stop();
+					          }
+				        }
+			      }
+		    }
+	  });
 }
 
 
 //onload
 $(document).ready(function(){
-	window.history.pushState(null,null,'');
-	//grain display init
-	var canvas2 = document.getElementById('canvas2');
-	var processing = new Processing(canvas2,grainsdisplay);
+	  window.history.pushState(null,null,'');
+	  //grain display init
+	  var canvas2 = document.getElementById('canvas2');
+	  var processing = new Processing(canvas2,grainsdisplay);
 
-	document.addEventListener("touchmove",function(e){
-		e.preventDefault();
-	});
-	//gui
-	guiinit();
-	
+	  document.addEventListener("touchmove",function(e){
+		    e.preventDefault();
+	  });
+	  //gui
+	  guiinit();
+
 });
